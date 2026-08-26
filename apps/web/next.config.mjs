e3fc3d22719e873a0abd0c58127c34ce6135178a
Plumz <Konstantin.kunisch.dw@gmail.com>
2026-08-26 09:58:03 +0200

    Update next.config.mjs

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001/api/v1',
   },
 };
+
 export default nextConfig;
